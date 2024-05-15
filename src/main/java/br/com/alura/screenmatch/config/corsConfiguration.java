171d008c6c406cf8d2f1c43eca8a63fa97602c7a
gabrielnovaisgs <gabrielnovaisgs@gmail.com>
2024-05-15 16:19:05 -0300

    [fix]: fix configuration to cors and change port

--- src/main/java/br/com/alura/screenmatch/config/corsConfiguration.java
+++ src/main/java/br/com/alura/screenmatch/config/corsConfiguration.java
@@ -1,8 +1,10 @@
 package br.com.alura.screenmatch.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class corsConfiguration implements WebMvcConfigurer {
 
     @Override
