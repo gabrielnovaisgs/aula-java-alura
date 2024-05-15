0e1f116c17ccbdfb918277d3808fc58a0ba4ee16
gabrielnovaisgs <gabrielnovaisgs@gmail.com>
2024-05-14 21:26:15 -0300

    [feat]: return series

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,16 +1,32 @@
 package br.com.alura.screenmatch.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+
+import br.com.alura.screenmatch.repository.SerieRepository;
 
 @RestController
 public class SerieController {
+    @Autowired
+    private SerieRepository repositorio;
 
     @GetMapping("/series")
-    public String helloWorld() {
-        return "Hello world";
+    public List<SerieDTO> obterSeries() {
+        return repositorio.findAll().stream().map(s -> new SerieDTO(
+                s.getId(),
+                s.getTitulo(),
+                s.getTotalTemporadas(),
+                s.getAvaliacao(),
+                s.getGenero(),
+                s.getAtores(),
+                s.getPoster(),
+                s.getSinopse())).collect(Collectors.toList());
     }
 
 }
